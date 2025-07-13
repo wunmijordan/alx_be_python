449f2aabba5d8377b16f15f24c43bb5b728a4fff
Wunmi <wunmijordan@gmail.com>
2025-07-13 02:21:46 +0100

    Add basic_operations.py to python_introduction directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+Sum = number1 + number2
+Difference = number1 - number2
+Product = number1 * number2
+
+print("Addition of", number1, "and", number2, "is", Sum)
+print("Subtraction of", number1, "amd", number2, "is", Difference)
+print("Multiplication of", number1, "and", number2, "is", Product)
