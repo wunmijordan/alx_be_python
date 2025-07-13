038269ba836d28d4fae5ebf8b2f5309fff8ad989
Wunmi <wunmijordan@gmail.com>
2025-07-13 02:34:33 +0100

    Add simple_interest.py to python_introduction directory

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000  # Principal amount in dollars
+rate = 0.05  # Interest rate in percent
+time = 3  # Time in years
+
+interest = principal * rate * time  # Simple interest formula
+
+print("The simple interest is:", interest)
