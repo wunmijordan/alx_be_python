498def4a5fb68928ab0dceabb558196801a4b63e
Wunmi <wunmijordan@gmail.com>
2025-07-13 03:11:18 +0100

    Fix: corrected calculation logic in hours_to_seconds.py

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,5 +1,5 @@
 hours = 2
 
-seconds = (3600 * hours) # Convert hours to seconds
+seconds = (hours * 60 * 60) # Convert hours to seconds
 
 print(hours, "hours(s) is", seconds, "seconds.")  # Output the result
